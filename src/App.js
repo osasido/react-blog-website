9ccb45d05bafe61385fda188573c9d17c0d862f8
osasido <trustirooj@gmail.com>
2022-04-17 01:39:20 +0100

    blog

--- src/App.js
+++ src/App.js
@@ -1,73 +1,11 @@
-import { useCallback,useState,useEffect  } from 'react' 
-import {BiCalendar} from 'react-icons/bi'
-import Search from './components/Search'
-import AppointmentInfo from './components/Appointmentinfo'
-import AddAppointment from './components/AddAppointment'
-
-
-export default function App() {
-  let [appointmentList, setAppointmentList] =useState([])
-let [query, setQuery] = useState("");
-let [sortBy, setSortBy] = useState("petName");
-let [orderBy, setOrderBy] = useState("asc");
-const fetchData = useCallback(() => {
-  fetch('./data.json')
-    .then(response => response.json())
-    .then(data => {
-      setAppointmentList(data)
-    });
-}, [])
-
-useEffect(() => {
-  fetchData()
-}, [fetchData]);
-const filteredAppointments = appointmentList.filter(
-  item => {
+import React, { Component } from 'react'
+import HomePage from './pages/Homepage'
+export default class App extends Component {
+  render() {
     return (
-      item.petName.toLowerCase().includes(query.toLowerCase()) ||
-      item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-      item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      <div>
+        <HomePage/>
+      </div>
     )
   }
-).sort((a, b) => {
-  let order = (orderBy === 'asc') ? 1 : -1;
-  return (
-    a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
-      ? -1 * order : 1 * order
-  )
-})
-
-
-  return (
-    <div className="App container mx-auto mt-3 font-thin">
-    <h1 className="text-5xl mb-3">
-      <BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
-    <AddAppointment
-      onSendAppointment={myAppointment => setAppointmentList([...appointmentList, myAppointment])}
-      lastId={appointmentList.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
-    />
-    <Search query={query}
-      onQueryChange={myQuery => setQuery(myQuery)}
-      orderBy={orderBy}
-      onOrderByChange={mySort => setOrderBy(mySort)}
-      sortBy={sortBy}
-      onSortByChange={mySort => setSortBy(mySort)}
-    />
-
-    <ul className="divide-y divide-gray-200">
-      {filteredAppointments
-        .map(appointment => (
-          <AppointmentInfo key={appointment.id}
-            appointment={appointment}
-            onDeleteAppointment={
-              appointmentId =>
-                setAppointmentList(appointmentList.filter(appointment =>
-                  appointment.id !== appointmentId))
-            }
-          />
-        ))
-      }
-    </ul>
-  </div>
-  )
 }
